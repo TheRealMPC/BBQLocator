1471fc3a9cfe6aa673a8321736556405d59d7c6e
TheRealMPC <smkscar25@hotmail.com>
2017-08-01 16:08:14 -0500

    noserver-project ver2 upload

--- js/locationsCtrl.js
+++ js/locationsCtrl.js
@@ -1,5 +1,5 @@
 angular.module('bbq').controller('locationsCtrl', function($scope, $http, mainService) {
-  $http.get('data.json').then(
+  $http.get('./data.json').then(
       function(locations){
         $scope.locations = locations.data;
       }
